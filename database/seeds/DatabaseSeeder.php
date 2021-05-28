adcc73f20e9e49b1d5fce7b6f2e3789033a1b5a4
Kheyzel <kheyzelnugoy@gmail.com>
2021-05-28 22:53:30 +0800

    add contact seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('contacts')->insert([
+            'contact_number' => '+639218362697',
+            'contact_number1' => '+639669681402',
+            'bank_name' => 'Bank of Philippines Island (BPI)',
+            'bank_branch' => '0342 Sta. Cruz, Laguna',
+            'bank_act_num' => '003423-22482-37',
+            'swift_code_num' => 'BOPI PHMM',
+            'email_ad' => 'margaret7317@gmail.com',
+            'address' => 'NHA Southville 7, Site 2, Brgy. Dayap, Calauan, Laguna'
+        ]);
     }
 }
