57f622da49d93194b7ba20fb76e35506577dd53c
Kheyzel <kheyzelnugoy@gmail.com>
2021-05-29 00:09:08 +0800

    edit act_image name in calendar controoler.. just a typo

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -28,7 +28,7 @@ class CalendarController extends Controller
             {
                 $file = $request->file('act_image');
                 $filename =$file->getClientOriginalName();
-                $request-> datact_image->storeAs('gallery_image',$filename,'public');
+                $request-> act_image->storeAs('gallery_image',$filename,'public');
                 $calendar->act_image=$filename;
             }
             $calendar->save();
